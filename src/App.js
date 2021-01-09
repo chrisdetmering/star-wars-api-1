bcc8be8226e5bce725c90eb0fea93bc38795cca2
Cesar Preza <cpreza93@yahoo.com>
2021-01-09 10:14:03 -0800

    Async/Await

--- src/App.js
+++ src/App.js
@@ -15,27 +15,32 @@ class App extends Component {
     }
   }
 
-componentDidMount() {
-  let people = `https://swapi.dev/api/people/`
-  let homeWorld = 'https://swapi.dev/api/planets/'
-  const peopleRequest = axios.get(people)
-  const homeWorldRequest = axios.get(homeWorld)
-  axios.all([peopleRequest, homeWorldRequest])
-  .then(
-    axios.spread((...responses) => {
-      const peopleResponse = responses[0];
-      const homeWorldResponse = responses[1];
-      console.log(peopleResponse, homeWorldResponse)
-      this.setState({
-        characters: peopleResponse.data.results,
-        homeWorld: homeWorldResponse.data.results
-      })
-    })
-  )
-  .catch(err => {
-    console.log(err)
-  })
-} 
+async componentDidMount() {
+  const people = await axios.get(`https://swapi.dev/api/people/`)
+  console.log(people)
+}
+
+// componentDidMount() {
+//   const people = `https://swapi.dev/api/people/`
+//   const homeWorld = 'https://swapi.dev/api/planets/'
+//   const peopleRequest = axios.get(people)
+//   const homeWorldRequest = axios.get(homeWorld)
+//   axios.all([peopleRequest, homeWorldRequest])
+//   .then(
+//     axios.spread((...responses) => {
+//       const peopleResponse = responses[0];
+//       const homeWorldResponse = responses[1];
+//       console.log(peopleResponse, homeWorldResponse)
+//       this.setState({
+//         characters: peopleResponse.data.results,
+//         homeWorld: homeWorldResponse.data.results
+//       })
+//     })
+//   )
+//   .catch(err => {
+//     console.log(err)
+//   })
+// } 
 
 render() {
   console.log(this.state.isLoading)
