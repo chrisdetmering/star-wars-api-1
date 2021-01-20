aaa36ddf27c2f46b2446da647799c06e41d14ae4
Cesar Preza <cpreza93@yahoo.com>
2021-01-19 20:41:37 -0800

    styling

--- src/components/CharacterTable.js
+++ src/components/CharacterTable.js
@@ -8,32 +8,32 @@ class CharacterTable extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.props.isLoading ? <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Birth Date</th>
-                            <th>Height</th>
-                            <th>Mass</th>
-                            <th>Homeworld</th>
-                            <th>Species</th>    
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.characterData.map(data => 
-                            <tr key={data.name}>
-                                <td> {data.name} </td>
-                                <td> {data.birth_year} </td>
-                                <td> {data.height}cm </td>
-                                <td> {data.mass}kg </td>
-                                <td> {data.homeworld} </td> 
-                                <td> {data.species} </td>
-                            </tr>)}
-                    </tbody>
-                </table> : <ReactBootstrap.Spinner animation="border" /> }
-                
-
+            <div className='table-container'>
+                {this.props.isLoading ?
+                    <table className='table table-bordered'>
+                        <thead className="text-light">
+                            <tr>
+                                <th>Name</th>
+                                <th>Birth Date</th>
+                                <th>Height</th>
+                                <th>Mass</th>
+                                <th>Homeworld</th>
+                                <th>Species</th>    
+                            </tr>
+                        </thead>
+                        <tbody className="text-light">
+                            {this.props.characterData.map(data => 
+                                <tr key={data.name}>
+                                    <td> {data.name} </td>
+                                    <td> {data.birth_year} </td>
+                                    <td> {data.height}cm </td>
+                                    <td> {data.mass}kg </td>
+                                    <td> {data.homeworld} </td> 
+                                    <td> {data.species} </td>
+                                </tr>)}
+                        </tbody>
+                        </table> :
+                    <ReactBootstrap.Spinner animation="border" />}
             </div>
         )
     }
